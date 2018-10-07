f2c55e02850e4c7b86ab41e8a71a5078c6c79fc3
wangchongbei <wchb20155@gmail.com>
2018-10-07 15:35:11 -0700

    update

--- src/main/java/com/wchb/leetcode/S48.java
+++ src/main/java/com/wchb/leetcode/S48.java
@@ -5,19 +5,34 @@ package com.wchb.leetcode;
  */
 public class S48 {
 
+    /*
+     * clockwise rotate
+     * first reverse up to down, then swap the symmetry
+     * 1 2 3     7 8 9     7 4 1
+     * 4 5 6  => 4 5 6  => 8 5 2
+     * 7 8 9     1 2 3     9 6 3
+     */
+
     //先将上下翻转，再沿对角线翻转
     public void rotate(int[][] matrix) {
 
         int row = matrix.length;
         int col = matrix[0].length;
 
-        //第一行变最后一列
-        for (int i = 0; i < row / 2; i++) {
-            for (int j = 0; j < col; j++) {
-                swap(matrix, i, j, row - i - 1, j);
-            }
-        }
+        //reverse up to down
 
+        int up = 0;
+        int down = row - 1;
+
+        while (up < down) {
+
+            int[] temp = matrix[up];
+            matrix[up] = matrix[down];
+            matrix[down] = temp;
+
+            up++;
+            down--;
+        }
 
         for (int i = 0; i < row; i++) {
             for (int j = i + 1; j < col; j++) {
@@ -32,48 +47,13 @@ public class S48 {
         matrix[x1][y1] = temp;
     }
 
-    /************************************************************/
+    /*
+     * anticlockwise rotate
+     * first reverse left to right, then swap the symmetry
+     * 1 2 3     3 2 1     3 6 9
+     * 4 5 6  => 6 5 4  => 2 5 8
+     * 7 8 9     9 8 7     1 4 7
+     */
 
-    public void rotateV2(int[][] matrix) {
-
-        if (matrix.length == 0 || matrix.length != matrix[0].length) return;
-        int n = matrix.length;
-
-        for (int layer = 0; layer < n / 2; layer++) {
-            int first = layer;
-            int last = n - 1 - layer;
-            for (int i = first; i < last; i++) {
-                int offset = i - first;
-
-
-                //top   :matrix[first][i]
-                //bottom:matrix[last][last - offset]
-
-                //left  :matrix[last - offset][first]
-                //right :matrix[i][last]
-
-                int top = matrix[first][i];//save top
-                //left->top
-                matrix[first][i] = matrix[last - offset][first];
-                //bottom->left
-                matrix[last - offset][first] = matrix[last][last - offset];
-                //right->bottom
-                matrix[last][last - offset] = matrix[i][last];
-                //top->right
-                matrix[i][last] = top;
-            }
-        }
-
-//        //原地坐标转换
-//        for (int i = 0; i < height / 2; i++) {
-//            for (int j = i; j < height - 1 - i; j++) {
-//                temp = matrix[i][j];
-//                matrix[i][j] = matrix[height - j - 1][i];
-//                matrix[height - j - 1][i] = matrix[height - i - 1][height - j - 1];
-//                matrix[height - i - 1][height - j - 1] = matrix[j][height - i - 1];
-//                matrix[j][height - i - 1] = temp;
-//            }
-//        }
-    }
 
 }
