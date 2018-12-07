0074864ef994032bfe3d8e091bbe53d334af84e7
wangchongbei <wchb20155@gmail.com>
2018-12-07 07:39:48 -0800

    update

--- src/main/java/com/wchb/leetcode/S399.java
+++ src/main/java/com/wchb/leetcode/S399.java
@@ -3,70 +3,58 @@ package com.wchb.leetcode;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @date 9/20/18 8:56 PM
  */
 public class S399 {
 
-    int n;
-    Map<String, HashMap<String, Double>> g;
-
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        int n = equations.length;
 
-        n = values.length;
-        if (n == 0) return new double[]{};
-        g = new HashMap<>();
-        double[] ans = new double[queries.length];
+        //build graph
+        Map<String, Map<String, Double>> g = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String from = equations[i][0];
+            String to = equations[i][1];
+            double val = values[i];
 
-        for (int i = 0; i < n; ++i) {
-            String x = equations[i][0];
-            String y = equations[i][1];
-            double k = values[i];
-            if (!g.containsKey(x)) {
-                HashMap<String, Double> map = new HashMap<>();
-                map.put(y, k);
-                g.put(x, map);
-            } else {
-                g.get(x).put(y, k);
-            }
-            if (!g.containsKey(y)) {
-                HashMap<String, Double> map = new HashMap<>();
-                map.put(x, 1.0d / k);
-                g.put(y, map);
-            } else {
-                g.get(y).put(x, 1.0d / k);
-            }
-//            g.computeIfAbsent(x, l -> new HashMap<>()).put(y, k);
-//            g.computeIfAbsent(y, l -> new HashMap<>()).put(x, 1.0 / k);
+            if (!g.containsKey(from)) g.put(from, new HashMap<>());
+            if (!g.containsKey(to)) g.put(to, new HashMap<>());
+
+
+            g.get(from).put(to, val);
+            g.get(to).put(from, 1.0 / val);
         }
 
+        //do dfs
+        double[] ans = new double[queries.length];
 
-        for (int i = 0; i < queries.length; ++i) {
-            String from = queries[i][0];
-            String to = queries[i][1];
-            if (!g.containsKey(from) || !g.containsKey(to)) {
-                ans[i] = -1.0;
-            } else {
-                ans[i] = dfs(from, to, new HashSet<>());
-            }
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = dfs(g, queries[i][0], queries[i][1], new HashSet<>());
         }
 
         return ans;
     }
 
-    private double dfs(String from, String to, HashSet<String> visited) {
+    private double dfs(Map<String, Map<String, Double>> g, String from, String to, Set<String> visited) {
+
+        if (!g.containsKey(from) || !g.containsKey(to)) return -1.0;
+
         if (from.equals(to)) return 1.0;
         visited.add(from);
-        if (!g.containsKey(from)) return -1.0d;
-        for (String n : g.get(from).keySet()) {
+
+        Map<String, Double> adjs = g.get(from);
+
+        for (String n : adjs.keySet()) {
             if (visited.contains(n)) continue;
             visited.add(n);
-            double d = dfs(n, to, visited);
+            double d = dfs(g, n, to, visited);
             if (d > 0) return d * g.get(from).get(n);
         }
+
         return -1.0;
     }
-
 
 }
