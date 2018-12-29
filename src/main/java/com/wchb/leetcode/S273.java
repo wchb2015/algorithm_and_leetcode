76114843952f024642d34dfcc5c6ef80a34732c0
wangchongbei <wchb20155@gmail.com>
2018-12-28 16:35:29 -0800

    update   }

---
+++ src/main/java/com/wchb/leetcode/S273.java
@@ -0,0 +1,29 @@
+package com.wchb.leetcode;
+
+/**
+ * @date 12/28/18 11:04 AM
+ */
+public class S273 {
+    private final String[] belowTen = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private final String[] belowTwenty = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] belowHundred = new String[]{"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        StringBuilder sb = new StringBuilder();
+
+        if (num < 1000) {
+
+        } else if (num < 1000000) {
+
+        } else if (num < 1000000000) {
+
+        }
+
+
+        return "";
+
+    }
+
+
+}
